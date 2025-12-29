934f6f01d64e19d68d572a38d6913a5362ec1da7
LINKMASTER\conguchu <anton.davila.carro@gmail.com>
2025-12-29 09:17:35 +0100

    creacion proyecto springboot

---
+++ src/test/java/anton/teis/incidencias/GestorDeIncidenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package anton.teis.incidencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeIncidenciasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
