a8eb9211b9836057a49360b184f47daac150e788
a22ricardoadc <a22ricardoadc@iesteis.es>
2026-01-08 12:44:37 +0100

    (feat) validaciones en los campos para hacerlos unicos y no nullables

--- src/main/java/anton/teis/incidencias/entity/user/Usuarios.java
+++ src/main/java/anton/teis/incidencias/entity/user/Usuarios.java
@@ -14,10 +14,14 @@ public abstract class Usuarios {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nombre;
+    @Column(nullable = false)
     private String apellidos;
 
+    @Column(unique = true, nullable = false)
     private String username;
+    @Column(nullable = false)
     private String password;
 
     private boolean alta = true; // si no esta dado de alta no aparecerá en el front-end
