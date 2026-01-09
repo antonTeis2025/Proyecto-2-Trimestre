ee659aeb5913be015edd5d4d2ddfdb759c6c5477
a22ricardoadc <a22ricardoadc@iesteis.es>
2026-01-09 13:17:25 +0100

    (feat) DTO para incidencias

---
+++ src/main/java/anton/teis/incidencias/dto/IncidenciaData.java
@@ -0,0 +1,18 @@
+package anton.teis.incidencias.dto;
+
+
+import anton.teis.incidencias.entity.incidencia.Tipo;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class IncidenciaData {
+    private long user_id;
+    @NotNull
+    @Size(min = 1, max = 200, message = "Como máximo la descripción ha de tener 200 caracteres")
+    private String descripcion;
+    private String IP;
+    @NotNull(message = "El tipo de incidencia es obligatorio y debe ser uno de los permitidos (OTRO, HARDWARE, SOFTWARE, RED, ERROR)")
+    private Tipo tipo;
+}
