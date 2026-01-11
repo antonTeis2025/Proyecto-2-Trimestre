99e62f0cda6fa44698ca52cdecdcc20ca55e380e
LINKMASTER\conguchu <anton.davila.carro@gmail.com>
2026-01-11 09:17:39 +0100

    (feat) metodos para obtener incidencias por id de tecnico

--- src/main/java/anton/teis/incidencias/repository/incidencia/IncidenciaResueltaRepository.java
+++ src/main/java/anton/teis/incidencias/repository/incidencia/IncidenciaResueltaRepository.java
@@ -2,6 +2,13 @@ package anton.teis.incidencias.repository.incidencia;
 
 import anton.teis.incidencias.entity.incidencia.IncidenciaResuelta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface IncidenciaResueltaRepository extends JpaRepository<IncidenciaResuelta, Long> {
+
+    @Query("SELECT i FROM IncidenciaResuelta i JOIN FETCH i.tecnicos t WHERE t.id = :id")
+    List<IncidenciaResuelta> findByTecnicoId(long id);
+
 }
