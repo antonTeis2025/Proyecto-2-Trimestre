b3d877df6e40ce726db1c75969c3d894664638c0
LINKMASTER\conguchu <anton.davila.carro@gmail.com>
2026-01-11 09:17:20 +0100

    (feat) DTO para el endpoint que devolvera las incidencias por id de tecnico

---
+++ src/main/java/anton/teis/incidencias/dto/IncidenciasTecnicoResponse.java
@@ -0,0 +1,17 @@
+package anton.teis.incidencias.dto;
+
+import anton.teis.incidencias.entity.incidencia.IncidenciaCerrada;
+import anton.teis.incidencias.entity.incidencia.IncidenciaEnProceso;
+import anton.teis.incidencias.entity.incidencia.IncidenciaResuelta;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class IncidenciasTecnicoResponse {
+    private List<IncidenciaEnProceso> enProceso;
+    private List<IncidenciaResuelta> resueltas;
+    private List<IncidenciaCerrada> cerradas;
+}
--- src/main/java/anton/teis/incidencias/repository/incidencia/IncidenciaEnProcesoRepository.java
+++ src/main/java/anton/teis/incidencias/repository/incidencia/IncidenciaEnProcesoRepository.java
@@ -3,5 +3,8 @@ package anton.teis.incidencias.repository.incidencia;
 import anton.teis.incidencias.entity.incidencia.IncidenciaEnProceso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IncidenciaEnProcesoRepository extends JpaRepository<IncidenciaEnProceso, Long> {
+    List<IncidenciaEnProceso> findByTecnico_Id(Long id);
 }
