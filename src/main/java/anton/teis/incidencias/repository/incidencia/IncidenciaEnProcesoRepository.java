5f55d79966a8f453827639046edb0cbcbd6a083d
LINKMASTER\conguchu <anton.davila.carro@gmail.com>
2026-01-12 15:45:59 +0100

    (feat) metodo para obtener todas las incidencias que NO sean de un tecnico por ID

--- src/main/java/anton/teis/incidencias/repository/incidencia/IncidenciaEnProcesoRepository.java
+++ src/main/java/anton/teis/incidencias/repository/incidencia/IncidenciaEnProcesoRepository.java
@@ -4,11 +4,16 @@ import anton.teis.incidencias.entity.incidencia.IncidenciaAbierta;
 import anton.teis.incidencias.entity.incidencia.IncidenciaEnProceso;
 import anton.teis.incidencias.entity.user.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface IncidenciaEnProcesoRepository extends JpaRepository<IncidenciaEnProceso, Long> {
     List<IncidenciaEnProceso> findByTecnico_Id(Long id);
     List<IncidenciaEnProceso> findByUsuario(Usuario usuario);
+
+    @Query("SELECT i FROM IncidenciaEnProceso i WHERE i.tecnico.id <> :idTecnico")
+    List<IncidenciaEnProceso> findAllOtrosTecnicos(@Param("idTecnico") Long idTecnico);
 
 }
