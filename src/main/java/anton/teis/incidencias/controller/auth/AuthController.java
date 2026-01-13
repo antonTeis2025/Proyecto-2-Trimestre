684bfe472d8fea29195eedea312f122fd5fba977
Anton DC <a22ricardoadc@iesteis.es>
2026-01-13 13:03:58 +0100

    (feat) endpoint para obtener los jwt

---
+++ src/main/java/anton/teis/incidencias/controller/auth/AuthController.java
@@ -0,0 +1,48 @@
+package anton.teis.incidencias.controller.auth;
+
+import anton.teis.incidencias.security.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    /**
+     * Al hacer un post a /api/auth/login con username y passwd en json
+     * devuelve un jwt valido con una validez de 10 dias
+     * @param credenciales
+     * @return
+     */
+    
+    @PostMapping("/login")
+    public Map<String, String> login(@RequestBody Map<String, String> credenciales) {
+        String username = credenciales.get("username");
+        String password = credenciales.get("password");
+
+        // autentica con spring segurity
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(username, password)
+        );
+        // si la autenticacion funciona, genera el token
+        if (authentication.isAuthenticated()) {
+            String token = jwtUtils.generateToken(username);
+            return Map.of("token", token); // { "token": "" }
+        } else {
+            throw new RuntimeException("Credenciales inválidas");
+        }
+    }
+}
