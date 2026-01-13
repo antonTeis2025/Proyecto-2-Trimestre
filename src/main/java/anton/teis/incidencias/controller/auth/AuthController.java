2ae0638c7fee369f7faac7dcd83d26b607cec2dd
Anton DC <a22ricardoadc@iesteis.es>
2026-01-13 14:00:33 +0100

    (fix) cambiado a tipo controller para que devuelva paginas web

--- src/main/java/anton/teis/incidencias/controller/auth/AuthController.java
+++ src/main/java/anton/teis/incidencias/controller/auth/AuthController.java
@@ -5,11 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@RestController
+@Controller
 public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -24,6 +25,7 @@ public class AuthController {
      * @return
      */
     @PostMapping("/api/auth/login")
+    @ResponseBody
     public Map<String, String> login(@RequestBody Map<String, String> credenciales) {
         String username = credenciales.get("username");
         String password = credenciales.get("password");
